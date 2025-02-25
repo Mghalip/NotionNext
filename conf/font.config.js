4b81e5c4e000a9c7fa234840cda1eadd5262c686
Mghalip <mahprat0510@gmail.com>
2025-02-25 23:25:51 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -8,36 +8,22 @@ module.exports = {
   FONT_URL: [
     'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap', // 添加 Lato 字体链接
   ],
-  // 无衬线字体 例如'"Lato"'
+  // 无衬线字体 例如 '"Lato"'
   FONT_SANS: [
     '"Lato"',  // 使用 Lato 作为无衬线字体
     '-apple-system',
     'BlinkMacSystemFont',
-    '"Hiragino Sans GB"',
-    '"Microsoft YaHei"',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
     '"Segoe UI"',
-    '"Noto Sans SC"',
-    'HarmonyOS_Regular',
     '"Helvetica Neue"',
     'Helvetica',
-    '"Source Han Sans SC"',
-    'Arial',
+    '"Arial"',
     'sans-serif',
-    '"Apple Color Emoji"'
   ],
-  // 衬线字体 例如'"Times New Roman"'
+  // 衬线字体 例如 '"Times New Roman"'
   FONT_SERIF: [
-    'Bitter',
-    '"Noto Serif SC"',
-    'SimSun',
     '"Times New Roman"',
-    'Times',
+    'Georgia',
     'serif',
-    '"Segoe UI Emoji"',
-    '"Segoe UI Symbol"',
-    '"Apple Color Emoji"'
   ],
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
