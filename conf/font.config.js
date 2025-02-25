01693e7370e16fb525ed93320d76c4f695270471
Mghalip <mahprat0510@gmail.com>
2025-02-25 23:22:19 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -4,20 +4,13 @@
  */
 module.exports = {
   // START ************网站字体*****************
-  // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
-  // 后面空格隔开的font-light的字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light',
-  // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
+  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-light', // 可以继续使用 font-light 调整粗细
   FONT_URL: [
-    // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
-    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
+    'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap', // 添加 Lato 字体链接
   ],
-  // 无衬线字体 例如'"LXGW WenKai"'
+  // 无衬线字体 例如'"Lato"'
   FONT_SANS: [
-    // '"LXGW WenKai"',
-    '"PingFang SC"',
+    '"Lato"',  // 使用 Lato 作为无衬线字体
     '-apple-system',
     'BlinkMacSystemFont',
     '"Hiragino Sans GB"',
@@ -34,9 +27,8 @@ module.exports = {
     'sans-serif',
     '"Apple Color Emoji"'
   ],
-  // 衬线字体 例如'"LXGW WenKai"'
+  // 衬线字体 例如'"Times New Roman"'
   FONT_SERIF: [
-    // '"LXGW WenKai"',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
@@ -49,7 +41,6 @@ module.exports = {
   ],
   FONT_AWESOME:
     process.env.NEXT_PUBLIC_FONT_AWESOME_PATH ||
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
-
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css' // 保持 font-awesome 字体图标地址
   // END ************网站字体*****************
 }
